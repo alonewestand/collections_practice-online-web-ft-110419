cd741b52fcf39da2cc43e0b41d775a1ef415e0e0
Ken Goad <goadke@g.cofc.edu>
2019-11-08 23:12:12 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
+require "pry"
 
+def sort_array_asc(array)
+  reverse_array = array.reverse
+  reverse_array
+end
+
+def sort_array_desc(array)
+  desc_array = array.sort.reverse
+  desc_array
+end
+
+def sort_array_char_count(array)
+ char_array = sort_array_char_count(array).each.length.sort.reverse
+ binding.pry
+ char_array
+end
