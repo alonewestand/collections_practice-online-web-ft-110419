e39ca71c94401b6c16b15fd5fe167b0866613046
Ken Goad <goadke@g.cofc.edu>
2019-11-08 23:14:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,17 +1,71 @@
-require "pry"
+require 'pry'
 
 def sort_array_asc(array)
-  reverse_array = array.reverse
-  reverse_array
+  array.sort
 end
 
 def sort_array_desc(array)
-  desc_array = array.sort.reverse
-  desc_array
+  array.reverse.sort
+  end
 end
 
 def sort_array_char_count(array)
- char_array = sort_array_char_count(array).each.length.sort.reverse
- binding.pry
- char_array
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.find_all do |word|
+    word[0] == "a"
+  end
+
+  # using select method
+    # array.select do |word|
+    #   word[0] == "a"
+    # end
+end
+
+def sum_array(array)
+  sum = 0
+  array.each do |num|
+    sum+=num
+  end
+  sum
+
+  # using reduce method
+    # array.reduce(:+)
+
+  # using inject method (short)
+     # array.inject(:+)
+
+  # using inject method (long)
+     # array.inject do |sum,x|
+     #  sum + x
+     # end
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
 end
